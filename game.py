874e004cf29d201fb6d75341307b55c25adb5bf9
Matthew Stebbins <mattcatt89@yahoo.com>
2023-05-20 10:05:50 -0500

    Co-authored-by: Charles Ludwig <CharlesWLudwig@users.noreply.github.com> Co-authored-by: judyjiang1 <judyjiang1@users.noreply.github.com>

--- game.py
+++ game.py
@@ -4,29 +4,60 @@ from random import randint
 """A number-guessing game."""
 
 # Put your code here
-name = str(input("Howdy, what's your name?"))
-print(f"{name}, I'm thinking of a number between 1 and 100.")
+def input_validation(text):
+    while not text.isdigit():
+        text = input("Invaild input! Please enter a number between 1 - 100:")
+    return text
 
-print("Try to guess my number.")
+done = False
+while not done:
+    best_score = 1000
+    guesses_left = 20
+    name = str(input("Howdy, what's your name?"))
+    print(f"{name}, I'm thinking of a number between 1 and 100.")
 
-your_guess = int(input("Your guess?"))
+    print("Try to guess my number.")
 
-my_number = randint(1, 100)
+    your_guess = int(input_validation(input("Your guess?")))
 
-counter = 1
+    my_number = randint(1, 100)
 
-while your_guess != my_number:
-    if your_guess < my_number:
-        print("Your guess is too low, try again.")
-        counter += 1
-        your_guess = int(input("Your guess? "))
+    counter = 1
+    guesses_left -= 1
 
-    elif your_guess > my_number:
-        print("Your guess is too high, try again.")
-        counter += 1
-        your_guess = int(input("Your guess? "))
+    while your_guess != my_number:
+        if guesses_left > 0: 
+            if your_guess < my_number:
+                print("Your guess is too low, try again.")
+                counter += 1
+                guesses_left -= 1
+                your_guess = int(input_validation(input("Your guess? ")))
 
-    else:
-        break
+            elif your_guess > my_number:
+                print("Your guess is too high, try again.")
+                counter += 1
+                guesses_left -= 1
+                your_guess = int(input_validation(input("Your guess? ")))
+
+            else:
+                break
+        else:
+            break
     
-print(f"Well done, {name}! You found my number in {counter} tries!")
+    if guesses_left == 0:
+        print("Too many tries!")
+    else:
+        print(f"Well done, {name}! \nYou found my number in {counter} tries! \nThe game still had {guesses_left} tries available!")
+
+    if counter < best_score:
+        best_score = counter
+
+    new_game = input("Would you like to play again? Y or N ")
+
+    new_game = new_game[0].capitalize()
+
+    if new_game == "N":
+        print(f"Your best score was {best_score} tries")    
+        done = True
+    else:
+        done = False
